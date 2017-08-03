891cd8626847600cf9ed205a57c14e9e020907ee
Applebaby <queeniezhang0810@gmail.com>
2017-08-02 22:37:02 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,9 @@
 function createTipsOptions() {
   var tips = document.getElementById("tips_array").value;
+  if(tips == ""){
+    alert("请输入小费选项");
+    return;
+  }
   var tips_array = tips.split(",");
   var length_tips = tips_array.length;
   var div_buttons = document.getElementById("tips_options_buttons");
@@ -11,7 +15,7 @@ function createTipsOptions() {
 }
 
 function createTipsOptionsButtons(tip){
-  var str_button = "<button type='button' onclick='createTip("+tip+")'>"+tip+"%</button>";
+  var str_button = "<button class='tip_button btn btn-success' type='button' onclick='createTip("+tip+")'>"+tip+"%</button>";
   return str_button;
 }
 
@@ -23,8 +27,8 @@ function createTip(tip) {
     }
     var calculate_tip = initial_price*(tip/100);
     var finial_price = parseFloat(initial_price) + parseFloat(calculate_tip);
-    var str_prices = "<label>您的初始价格：</label><div>"+initial_price+"</div>"
-    +"<label>您的小费：</label><div>"+calculate_tip.toFixed(2)+"</div>"
-    +"<label>您的最终价格：</label><div>"+finial_price.toFixed(2)+"</div>";
+    var str_prices = "<label>您的初始价格：</label><div class='price'>"+initial_price+"</div>"
+    +"<label>您的小费：</label><div class='price'>"+calculate_tip.toFixed(2)+"</div>"
+    +"<label>您的最终价格：</label><div class='price'>"+finial_price.toFixed(2)+"</div>";
     prices.innerHTML = str_prices;
 }
